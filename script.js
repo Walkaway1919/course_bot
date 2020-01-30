58b5d447f42e2f2feb81a360e9d9722ec731e3b0
Julia <walkaway1919@gmail.com>
2020-01-31 01:44:21 +0300

    task_bot_hard

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
 function playBot(){
   let botNum = Math.floor(Math.random() * 100);
-  console.dir( ask );
+  let count = 1;
   ask();
-
+console.dir(ask);
   function ask(){
     let userNum = prompt('Введите число от 1 до 100:');
     if (userNum === null) {
       alert('Игра закончена');
       return false;
     } else if (isNaN(userNum) || userNum.trim() == '') {
+      // (count - 1); - если мы не хотим считать это за попытку
       alert( 'Введите число!'); 
     } else if (userNum > botNum) {
-      alert('Загаданное число меньше!');
-    } else if (userNum < botNum){
-      alert('Загаданное число больше!');
+      if(count == 10){
+        if ( !confirm('Попытки закончились, хотите сыграть еще?') ){
+          alert('Спасибо за игру!');
+          return false;
+        }
+        botNum = Math.floor(Math.random() * 100);
+        count = 0;
+      } else {
+        alert(`Загаданное число меньше, осталось попыток ${10 - count}`);
+      }
+    } else if (userNum < botNum) {
+      if(count == 10){
+        if ( !confirm('Попытки закончились, хотите сыграть еще?') ){
+          alert('Спасибо за игру!');
+          return false;
+        }
+        botNum = Math.floor(Math.random() * 100);
+        count = 0;
+      } else {
+        alert(`Загаданное число больше, осталось попыток ${10 - count}`);
+      }
     } else if (userNum == botNum) {
-      alert('Правильно!');
+      if ( !confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?') ){
+        alert('Спасибо за игру!');
+        return false;
+      }
+      count = 0;
       botNum = Math.floor(Math.random() * 100);
     }
+    count++; 
     ask();
   }
 }
 
 playBot();
+
