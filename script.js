70d8685396b5a175b406de4d9ff9b261049cd4c4
Julia <walkaway1919@gmail.com>
2020-01-31 00:14:07 +0300

    task_bot

---
+++ script.js
@@ -0,0 +1,25 @@
+function playBot(){
+  let botNum = Math.floor(Math.random() * 100);
+  console.dir( ask );
+  ask();
+
+  function ask(){
+    let userNum = prompt('Введите число от 1 до 100:');
+    if (userNum === null) {
+      alert('Игра закончена');
+      return false;
+    } else if (isNaN(userNum) || userNum.trim() == '') {
+      alert( 'Введите число!'); 
+    } else if (userNum > botNum) {
+      alert('Загаданное число меньше!');
+    } else if (userNum < botNum){
+      alert('Загаданное число больше!');
+    } else if (userNum == botNum) {
+      alert('Правильно!');
+      botNum = Math.floor(Math.random() * 100);
+    }
+    ask();
+  }
+}
+
+playBot();
